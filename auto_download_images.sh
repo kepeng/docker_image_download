85783946b0dc8b90eec0ce1d86ea0515108832c8
kepeng <583939733@qq.com>
2020-08-21 12:23:52 +0800

    上传shell脚本

--- auto_download_images.sh
+++ auto_download_images.sh
@@ -24,5 +24,5 @@ do
   docker pull registry.cn-hangzhou.aliyuncs.com/kp_gcr_io/image:${image_version}
   echo "上传镜像到本地私库"
   docker tag registry.cn-hangzhou.aliyuncs.com/kp_gcr_io/image:${image_version} ${upload_image}
-  docker rmi registry.cn-hangzhou.aliyuncs.com/kp_gcr_io/image:${upload_image}
+  docker rmi registry.cn-hangzhou.aliyuncs.com/kp_gcr_io/image:${image_version}
 done
