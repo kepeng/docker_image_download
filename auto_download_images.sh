820aa13989550a54e12f9785b48088333cfa5331
kepeng <583939733@qq.com>
2020-08-21 12:02:40 +0800

    上传shell脚本

--- auto_download_images.sh
+++ auto_download_images.sh
@@ -1,28 +1,29 @@
-#docker login --username=583939733@qq.com registry.cn-hangzhou.aliyuncs.com
+#!bin/bash
 for image in `cat ./images_list.txt`
 do
   echo "begin download $image"
-  image=${1}
   tmp_image=${image#*\/}
-  echo tmp_image
-  image_version=${tmp_image//[\/:]/-}
-  echo image
-#  git checkout .
-#  git pull --force
-#  echo "From ${image}" > Dockerfile
-#  echo ">>>>>>>>>>>>>>>>>Dockerfile 已经变为 :" cat Dockerfile
-#  echo -e ">>>>>>>>>>>>>>>>>开始提交代码，下载镜像为: "${image}"\n"
-#  git add .
-#  git commit -m "add images '${image}'"
-#  git tag release-v${gen_image}
-#  git push --tags
-#  echo -e ">>>>>>>>>>>>>>>>>提交代码成功!\n"
-#  echo "等待120S"
-#  sleep 2m
-#  echo "开始下载镜像到本地"
-#  docker login --username=583939733@qq.com registry.cn-hangzhou.aliyuncs.com
-#  docker docker pull registry.cn-hangzhou.aliyuncs.com/kp_gcr_io/image:${image_version}
-#  echo "上传镜像到本地私库"
-#  docker tag registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${image_version} ${image}
-#  docker rmi registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${gen_image}
+  upload_image=192.168.174.130/gcr.io/${tmp_image}
+  echo ${tmp_image}
+  echo ${upload_image}
+  image_version=${tmp_image//[\:\/]/-}
+  echo ${image_version}
+  git checkout .
+  git pull --force
+  echo "From ${image}" > Dockerfile
+  echo ">>>>>>>>>>>>>>>>>Dockerfile 已经变为 :" cat Dockerfile
+  echo -e ">>>>>>>>>>>>>>>>>开始提交代码，下载镜像为: "${image}"\n"
+  git add .
+  git commit -m "add images '${image}'"
+  git tag release-v${gen_image}
+  git push --tags
+  echo -e ">>>>>>>>>>>>>>>>>提交代码成功!\n"
+  echo "等待120S"
+  sleep 2m
+  echo "开始下载镜像到本地"
+  docker login --username=583939733@qq.com registry.cn-hangzhou.aliyuncs.com
+  docker docker pull registry.cn-hangzhou.aliyuncs.com/kp_gcr_io/image:${image_version}
+  echo "上传镜像到本地私库"
+  docker tag registry.cn-hangzhou.aliyuncs.com/kp_gcr_io/image:${image_version} ${image}
+  docker rmi registry.cn-hangzhou.aliyuncs.com/kp_gcr_io/image:${upload_image}
 done
